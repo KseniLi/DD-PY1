d81e59445a99364a01633e6fb82446aba122750b
KseniLi <115585943+KseniLi@users.noreply.github.com>
2022-11-19 16:42:54 +0300

    Update lab5_task3.py

--- lab5_task3.py
+++ lab5_task3.py
@@ -1,6 +1,6 @@
 import random
 def get_unique_list_numbers() -> list[int]:
-    list_ = random.sample(range(-10, 10), 15)
+    list_ = random.sample(range(-10, 11), 15)
     return list_
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
