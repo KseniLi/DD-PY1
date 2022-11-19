4e7beff4a8f4b35836277a3dab54dd9de9f3f3bd
KseniLi <115585943+KseniLi@users.noreply.github.com>
2022-11-19 16:45:40 +0300

    Update lab5_task5.py

--- lab5_task5.py
+++ lab5_task5.py
@@ -1,9 +1,10 @@
 import random
 import string
-def get_random_password():
+def get_random_password(n):
     c = string.ascii_letters
     z = string.digits
-    pas = random.sample(c + z, 8)
+    pas = random.sample(c + z, n)
     pas = ''.join(pas)
     return pas
-print(get_random_password())
+n = 8
+print(get_random_password(n))
